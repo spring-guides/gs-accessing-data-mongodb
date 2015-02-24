e89163e40c380990acc5a51db1f528c9deca41ac
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 13:47:04 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,9 +3,9 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application implements CommandLineRunner {
 
 	@Autowired
