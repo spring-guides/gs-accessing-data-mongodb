7191e86fb44dceeb511c8f3b28d91e747ba47928
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-12 14:20:04 +0100

    Polish

--- complete/src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
+++ complete/src/main/java/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
@@ -1,48 +1,46 @@
 package com.example.accessingdatamongodb;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class AccessingDataMongodbApplication implements CommandLineRunner {
-
-	@Autowired
-	private CustomerRepository repository;
+public class AccessingDataMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMongodbApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+	@Bean
+	ApplicationRunner runner(CustomerRepository repository) {
+		return args -> {
 
-		repository.deleteAll();
+			repository.deleteAll();
 
-		// save a couple of customers
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
+			// save a couple of customers
+			repository.save(new Customer("Alice", "Smith"));
+			repository.save(new Customer("Bob", "Smith"));
 
-		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
+			// fetch all customers
+			System.out.println("Customers found with findAll():");
+			System.out.println("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				System.out.println(customer);
+			}
+			System.out.println();
 
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
+			// fetch an individual customer
+			System.out.println("Customer found with findByFirstName('Alice'):");
+			System.out.println("--------------------------------");
+			System.out.println(repository.findByFirstName("Alice"));
 
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-
+			System.out.println("Customers found with findByLastName('Smith'):");
+			System.out.println("--------------------------------");
+			for (Customer customer : repository.findByLastName("Smith")) {
+				System.out.println(customer);
+			}
+		};
 	}
 
 }
--- complete/src/test/java/com/example/accessingdatamongodb/CustomerRepositoryTests.java
+++ complete/src/test/java/com/example/accessingdatamongodb/CustomerRepositoryTests.java
@@ -1,38 +1,22 @@
 package com.example.accessingdatamongodb;
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *	  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.domain.Example;
 
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 public class CustomerRepositoryTests {
 
 	@Container
@@ -46,7 +30,6 @@ public class CustomerRepositoryTests {
 
 	@BeforeEach
 	public void setUp() {
-
 		repository.deleteAll();
 
 		dave = repository.save(new Customer("Dave", "Matthews"));
@@ -56,27 +39,21 @@ public class CustomerRepositoryTests {
 
 	@Test
 	public void setsIdOnSave() {
-
 		Customer dave = repository.save(new Customer("Dave", "Matthews"));
-
 		assertThat(dave.id).isNotNull();
 	}
 
 	@Test
 	public void findsByLastName() {
-
 		List<Customer> result = repository.findByLastName("Beauford");
-
 		assertThat(result).hasSize(1).extracting("firstName").contains("Carter");
 	}
 
 	@Test
 	public void findsByExample() {
-
 		Customer probe = new Customer(null, "Matthews");
-
 		List<Customer> result = repository.findAll(Example.of(probe));
-
 		assertThat(result).hasSize(2).extracting("firstName").contains("Dave", "Oliver August");
 	}
+
 }
